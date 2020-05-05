0717be04cc83f43b06e835407c5db0e1c95be947
tyrainman427 <tyraineytech@gmail.com>
2020-05-05 00:48:43 -0400

    working on api

--- film/apis.py
+++ film/apis.py
@@ -1,9 +1,22 @@
 import requests
-import sqlite3
+import json
 
 def get_film():
-    film_list = {}
-    url = f"https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt0221027"
+    film_list = []
+
+    # f = open("film_data.json", "r")
+    with open("film_data.json") as f:
+        data = json.load(f)
+
+        for item in data:
+            print("ID: ", item['id'])
+
+
+
+    movie = ""
+
+
+    url = f"https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/{movie}"
 
     headers = {
         'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com",
@@ -14,8 +27,8 @@ def get_film():
 
     movie  = response.json()
 
+    return film_list
 
-    i = movie['id']
     # print(movie['year'])
     # print(movie['length'])
     # print(movie['rating'])
@@ -25,7 +38,7 @@ def get_film():
     # print(movie['trailer']['link'])
     # print(movie['cast']) # Loop thru this
 
-    return film_list
+
 
 def search_film(movie):
     movies_list = []
@@ -40,6 +53,24 @@ def search_film(movie):
     response = requests.request("GET", url, headers=headers)
     titles = response.json()
 
+    # f = open("film_data.json", "w")
+    # for i in range(len(titles['titles'])):
+    #     f.write(f"{titles['titles'][i]}")
+    # print("File saved")
+    # f.close()
+
     for i in range(len(titles['titles'])):
         movies_list.append(titles['titles'][i])
+        for x in movies_list:
+            with open('film_data.json', 'w') as json_file:
+                json.dump(x, json_file)
+
+
     return movies_list
+
+
+
+# write_file = '/Users/rainman/Desktop/scratch/python/django_project/staticfiles'
+#  with open(write_file,’w’, newline = ‘’) as f:
+#    writer = csv.writer(f, lineterminator = ‘\n’)
+#    writer.writerows(movies_list)
