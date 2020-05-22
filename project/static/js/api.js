bd8788ed7cb9e323d6d618d68088cb5c8a0a09ad
tyrainman427 <tyraineytech@gmail.com>
2020-05-22 02:42:56 -0400

    used javascript for api call

--- project/static/js/api.js
+++ project/static/js/api.js
@@ -7,7 +7,7 @@ $(document).ready(function (){
 });
 
 function getMovies(searchText){
-  axios.get('http://www.omdbapi.com/?s=' + searchText  + '&apikey=5aaeba44')
+  axios.get('https://www.omdbapi.com/?s=' + searchText  + '&apikey=5aaeba44')
     .then((response) => {
        console.log(response);
       let movies = response.data.Search
@@ -40,7 +40,7 @@ function movieSelected(id) {
 function getMovie() {
   let movieId = sessionStorage.getItem('movieId')
 
-  axios.get('http://www.omdbapi.com/?i=' + movieId  + '&apikey=5aaeba44')
+  axios.get('https://www.omdbapi.com/?i=' + movieId  + '&apikey=5aaeba44')
     .then((response) => {
        console.log(response);
       let movie = response.data
